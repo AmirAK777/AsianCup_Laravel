d2772829722bb36cf38580fa79e8bc02682081e6
AmirAK777 <contactezpro@gmail.com>
2024-03-12 12:14:27 +0100

    wip

--- resources/views/admin/adminhome.blade.php
+++ resources/views/admin/adminhome.blade.php
@@ -10,7 +10,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <p>{{ __("You're logged in as Admin!") }}</p>
-                    <div class="mt-4 flex flex-wrap">
+                    <div class="mt-4">
                         <a href="{{ route('matches.create') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Create Match</a>
                         <a href="{{ route('stades.create') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Create Stade</a>
                         <a href="{{ route('billets.create') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Create Billet</a>
@@ -19,7 +19,7 @@
                 </div>
             </div>
             <div class="mt-16">
-                <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
                     @foreach ($matches as $match)
                     <x-match-card :team1="$match->team1" :team2="$match->team2" :match="$match" :stade="$match->stade" />
                     @endforeach
--- resources/views/components/match-card.blade.php
+++ resources/views/components/match-card.blade.php
@@ -1,30 +1,51 @@
-<div class="scale-100 p-6 bg-white rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
-    <div class="flex justify-between items-center w-full">
-        <div class="flex items-center"> <!-- Ajout de la classe flex et items-center ici -->
-            <div class="mr-4"> <!-- Ajout de la classe mr-4 pour espacer le contenu -->
-                <div class="flex items-center"> <!-- Contenu aligné horizontalement -->
-                    <div class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
-                        <img src="{{ $team1->club_image }}" alt="{{ $team1->club_name }}">
-                    </div>
-                    <span class="ml-2">{{ $team1->club_name }}</span>
-                </div>
+<div class="products__item rounded-lg bg-white overflow-hidden shadow-sm sm:rounded-lg mx-auto" style="max-width: 300px;">
+    <div class="products__header p-4 h-14 flex"></div>
+
+    <a href="/fr/Premier-league-d-angleterre/{{ $match->slug }}/" class="products__content">
+        <div class="products__team mb-8 p-4 flex justify-between items-start h-32">
+            <div class="products__team-block flex flex-col font-medium text-center items-center flex-none w-32">
+                <img src="{{ $team1->club_image }}" alt="{{ $team1->club_name }}" class="h-20 md:h-32">
+                {{ $team1->club_name }}
             </div>
-            <div>
-                <span class="text-4xl font-bold mx-4">vs</span>
+            <div class="text-lg font-bold text-gray-700 opacity-35 flex-shrink-0 w-10 self-center">
+                vs
             </div>
-            <div class="ml-4"> <!-- Ajout de la classe ml-4 pour espacer le contenu -->
-                <div class="flex items-center justify-end"> <!-- Contenu aligné horizontalement -->
-                    <span>{{ $team2->club_name }}</span>
-                    <div class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full ml-2">
-                        <img src="{{ $team2->club_image }}" alt="{{ $team2->club_name }}">
-                    </div>
-                </div>
+            <div class="products__team-block flex flex-col font-medium text-center items-center flex-none w-32">
+                <img src="{{ $team2->club_image }}" alt="{{ $team2->club_name }}" class="h-20 md:h-32">
+                {{ $team2->club_name }}
             </div>
         </div>
-        <div class="info flex flex-col justify-end"> <!-- Ajout des classes flex, flex-col et justify-end pour aligner en bas et en colonne -->
-            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-dark">{{ $match->name }}</h2>
-            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-dark">{{ $match->date }}</h2>
-            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-dark">{{ $stade->name }}</h2>
-        </div>
+        <ul class="products__item-info p-4 mb-6">
+            <li class="mb-4 font-medium flex items-start h-32">
+                <img src="https://www.ticketkosta.com/images/soccer.svg" alt="{{ $match->team1 }} vs {{ $match->team2 }}" class="h-6 inline-block align-middle">
+                <span class="md:hidden inline-block ml-2">
+                    {{ $match->name }}
+                </span>
+            </li>
+            <li class="mb-4 font-medium flex items-start h-32">
+                <img src="https://www.ticketkosta.com/images/calendar.svg" alt="Date de match" class="h-6 inline-block align-middle">
+                <span class="md:hidden inline-block ml-2">
+
+                    {{ $match->date }}, {{ $match->time }}
+                </span>
+
+            </li>
+            <li class="mb-4 font-medium flex items-start h-32">
+                <img src="https://www.ticketkosta.com/images/location.svg" alt="Emplacement" class="h-6 inline-block align-middle">
+                <span class="md:hidden inline-block ml-2">
+
+                    {{ $match->stade->name }}
+                </span>
+
+            </li>
+        </ul>
+    </a>
+    <div class="products__footer flex px-4 pb-7 items-center"> <!-- Réduction de l'espacement horizontal -->
+        <a href="/fr/Premier-league-d-angleterre/{{ $match->slug }}/" class="btn bg-cyan-500 text-white rounded-full px-6 py-3 font-medium mr-5">
+            Voir les billets
+        </a>
+        <a class="products__like text-gray-600 text-2xl hover:text-red-500" href="#" data-item="{{ $match->id }}" data-action="add" data-additional="inlist">
+            <i class="far fa-heart"></i>
+        </a>
     </div>
 </div>
