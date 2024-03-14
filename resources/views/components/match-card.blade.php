50d0266867e30df34acd34f1360b3baef6d0b84f
AmirAK777 <contactezpro@gmail.com>
2024-03-14 10:58:21 +0100

    wip

--- resources/views/components/match-card.blade.php
+++ resources/views/components/match-card.blade.php
@@ -1,41 +1,34 @@
-<div class="products__item rounded-lg bg-white overflow-hidden shadow-sm sm:rounded-lg mx-auto" style="max-width: 300px;">
-    <div class="products__header p-4 h-14 flex"></div>
+<div class="products__item rounded-lg bg-white overflow-hidden shadow-sm sm:rounded-lg mx-auto max-w-md">
+    <div class="products__header p-4 h-14 flex items-center justify-center">
+        <h2 class="text-lg font-bold text-gray-800">{{ $match->name }}</h2>
+    </div>
 
-    <a href="{{ $match->slug }}/" class="products__content">
-        <div class="products__team mb-8 p-4 flex justify-between items-start h-32">
-            <div class="products__team-block flex flex-col font-medium text-center items-center flex-none w-32">
-                <img src="{{ $team1->club_image }}" alt="{{ $team1->club_name }}" class="h-20 md:h-32">
-                {{ $team1->club_name }}
+    <a href="{{ $match->slug }}/" class="products__content block hover:bg-gray-50 transition duration-300">
+        <div class="products__team p-4 flex justify-between items-center">
+            <div class="products__team-block flex flex-col items-center">
+                <img src="{{ $team1->club_image }}" alt="{{ $team1->club_name }}" class="h-20 md:h-32 object-contain">
+                <span class="text-sm font-medium text-gray-700">{{ $team1->club_name }}</span>
             </div>
-            <div class="text-lg font-bold text-gray-700 opacity-35 flex-shrink-0 w-10 self-center">
+            <div class="text-lg font-bold text-gray-700 opacity-50">
                 vs
             </div>
-            <div class="products__team-block flex flex-col font-medium text-center items-center flex-none w-32">
-                <img src="{{ $team2->club_image }}" alt="{{ $team2->club_name }}" class="h-20 md:h-32">
-                {{ $team2->club_name }}
+            <div class="products__team-block flex flex-col items-center">
+                <img src="{{ $team2->club_image }}" alt="{{ $team2->club_name }}" class="h-20 md:h-32 object-contain">
+                <span class="text-sm font-medium text-gray-700">{{ $team2->club_name }}</span>
             </div>
         </div>
-        <ul class="products__item-info p-4 mb-6">
-            <li class="mb-4 font-medium flex items-start h-32">
-                <img src="https://www.ticketkosta.com/images/soccer.svg" alt="{{ $match->team1 }} vs {{ $match->team2 }}" class="h-6 inline-block align-middle">
-                <span class="md:hidden inline-block ml-2">
-                    {{ $match->name }}
-                </span>
+        <ul class="products__item-info p-4 border-t border-gray-200">
+            <li class="mb-4 flex items-center">
+                <img src="https://www.ticketkosta.com/images/soccer.svg" alt="{{ $match->team1 }} vs {{ $match->team2 }}" class="h-6 inline-block align-middle mr-2">
+                <span class="text-gray-700">{{ $match->name }}</span>
             </li>
-            <li class="mb-4 font-medium flex items-start h-32">
-                <img src="https://www.ticketkosta.com/images/calendar.svg" alt="Date de match" class="h-6 inline-block align-middle">
-                <span class="md:hidden inline-block ml-2">
-
-                    {{ $match->date }}, {{ $match->time }}
-                </span>
-
+            <li class="mb-4 flex items-center">
+                <img src="https://www.ticketkosta.com/images/calendar.svg" alt="Date de match" class="h-6 inline-block align-middle mr-2">
+                <span class="text-gray-700">{{ $match->date }}, {{ $match->time }}</span>
             </li>
-            <li class="mb-4 font-medium flex items-start h-32">
-                <img src="https://www.ticketkosta.com/images/location.svg" alt="Emplacement" class="h-6 inline-block align-middle">
-                <span class="md:hidden inline-block ml-2">
-                    {{ $match->stade->name }}
-                </span>
-
+            <li class="mb-4 flex items-center">
+                <img src="https://www.ticketkosta.com/images/location.svg" alt="Emplacement" class="h-6 inline-block align-middle mr-2">
+                <span class="text-gray-700">{{ $match->stade->name }}</span>
             </li>
         </ul>
     </a>
